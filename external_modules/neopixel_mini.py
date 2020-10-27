e5c9f3fbb7bde9211888f830746b0ca101714021
Geoffrey Brunois <geoffrey.brunois@gmail.com>
2020-10-27 08:43:50 +0100

    🐛 fix neopixel random (#5)

--- external_modules/neopixel_mini.py
+++ external_modules/neopixel_mini.py
@@ -1,7 +1,8 @@
+from time import sleep
 from neopixel import NeoPixel as AdaNeoPixel
-import time
 
 ColorRGB = (int, int, int)
+
 
 class NeoPixel:
 
@@ -57,37 +58,37 @@ class NeoPixelEffects:
                 pixel_index = (i * 256 // self._neopixel.num_pixels) + j
                 self._neopixel.set_pixel_color(i, self.wheel(pixel_index & 255))
             self._neopixel.show()
-            time.sleep(wait)
+            sleep(wait)
 
-    @staticmethod
-    def hsl(h: int, s: int, l: int) -> ColorRGB:
-        """
-        :param h: Hue from 0 to 359
-        :param s: Saturation from 0 to 99
-        :param l: Luminance from 0 to 99
-        """
-        def hsl2rgb(_h: float, _x: float, _c: float):
-            if 0 <= _h < 60:
-                return _c, _x, 0
-            if 60 <= _h < 120:
-                return _x, _c, 0
-            if 120 <= _h < 180:
-                return 0, _c, _x
-            if 180 <= _h < 240:
-                return 0, _x, _c
-            if 240 <= _h < 300:
-                return _x, 0, _c
-            if 300 <= _h < 360:
-                return _c, 0, _x
-
-        s = s / 100.0
-        l = l / 100.0
-
-        chroma = (1.0 - abs(2.0 * l - 1.0)) * s
-        x = chroma * (1.0 - abs((h/60.0) % 2.0 - 1.0))
-        m = l - chroma / 2.0
-
-        r, g, b = hsl2rgb(h, x, chroma)
-        return round((r + m) * 255), round((g + m) * 255), round((b + m) * 255)
+    # @staticmethod
+    # def hsl(h: int, s: int, l: int) -> ColorRGB:
+    #     """
+    #     :param h: Hue from 0 to 359
+    #     :param s: Saturation from 0 to 99
+    #     :param l: Luminance from 0 to 99
+    #     """
+    #     def hsl2rgb(_h: float, _x: float, _c: float):
+    #         if 0 <= _h < 60:
+    #             return _c, _x, 0
+    #         if 60 <= _h < 120:
+    #             return _x, _c, 0
+    #         if 120 <= _h < 180:
+    #             return 0, _c, _x
+    #         if 180 <= _h < 240:
+    #             return 0, _x, _c
+    #         if 240 <= _h < 300:
+    #             return _x, 0, _c
+    #         if 300 <= _h < 360:
+    #             return _c, 0, _x
+    #
+    #     s = s / 100.0
+    #     l = l / 100.0
+    #
+    #     chroma = (1.0 - abs(2.0 * l - 1.0)) * s
+    #     x = chroma * (1.0 - abs((h/60.0) % 2.0 - 1.0))
+    #     m = l - chroma / 2.0
+    #
+    #     r, g, b = hsl2rgb(h, x, chroma)
+    #     return round((r + m) * 255), round((g + m) * 255), round((b + m) * 255)
 
     # TODO Add ease quadratic functions
--- samples/neopixel_random.py
+++ samples/neopixel_random.py
@@ -4,7 +4,6 @@ Repeatedly displays random colours onto the LED strip.
 """
 
 from random import randint
-from microbit import *
 
 # <Includes>
 from external_modules.move_mini import *
@@ -15,14 +14,14 @@ move_mini = MoveMini()
 while True:
     # Iterate over each LED in the strip
 
-    for pixel_id in range(0, len(move_mini.neopixel)):
+    for pixel_id in range(5):
         red = randint(0, 60)
         green = randint(0, 60)
         blue = randint(0, 60)
 
         # Assign the current LED a random red, green and blue value between 0 and 60
-        move_mini.neopixel[pixel_id] = (red, green, blue)
+        move_mini.neopixel.set_pixel_color(pixel_id, (red, green, blue))
 
-        # Display the current pixel data on the Neopixel strip
-        move_mini.neopixel.show()
-        sleep(100)
+    # Display the current pixel data on the Neopixel strip
+    move_mini.neopixel.show()
+    sleep(0.1)
